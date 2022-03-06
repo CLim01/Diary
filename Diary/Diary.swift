13daa65b8dd65e68f6a77f8c6a5948f6764b1d10
임성빈 <95527804+CLim01@users.noreply.github.com>
2022-03-06 21:08:57 +0900

    Add files via upload

---
+++ Diary/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 임성빈 on 2022/03/03.
+//
+
+import Foundation
+
+struct Diary {
+    var uuidString: String
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
